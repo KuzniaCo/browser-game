51361eaac6354cb652e77efca1a315d22f64a3b8
Bartłomiej Mont <spankertroll77@gmail.com>
2020-11-01 23:07:08 +0100

    Convert project to ts, improve project structure

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import path  from 'path';
+import express from 'express';
+import morgan from 'morgan';
+
+export const app:express.Application = express();
+
+import { router }  from './routes/index';
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+
