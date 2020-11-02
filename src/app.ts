e8776d6cb053c0218916ec0471aeefdead5d2a0f
Bartłomiej Mont <spankertroll77@gmail.com>
2020-11-02 23:14:33 +0100

    fix move public and view folders

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,14 @@ export const app = express();
 import { router }  from './routes/index';
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', router);
+
 
