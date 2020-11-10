4c31b6d8bb19434d4f75e91519277ecc4b5f559f
Patryk Orłowski <patrox199827@wp.pl>
2020-11-10 02:14:28 +0100

    character.ejs
    Add character.ejs with object via router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ export const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.render('character', { testejs: 'hi!' });
 });
 
