8c4db1fea4293e1a1cb13050c5939ab6d7c204db
Sienkowski <p.sienkowski@onet.pl>
2020-11-10 10:15:51 +0100

    added userInterface, changed export

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,19 @@
 import * as mongoose from "mongoose";
 
+export interface IUser extends mongoose.Document{
+    login: string,
+    email: string,
+    password: string,
+    registrationDate: Date,
+    lastLoggedIn: Date
+}
+
 const UserSchema = new mongoose.Schema({
   login: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, unique: true },
+  password: { type: String, required: true},
   registrationDate: Date,
   lastLoggedIn: Date,
 });
 
-const UserModel = mongoose.model("User", UserSchema);
-
-export {UserModel};
+export const User = mongoose.model<IUser>("User", UserSchema);
