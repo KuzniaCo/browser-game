721b83ac905efc04118133c95e64601e56bd6114
Maciej Ociepa <masiek457@gmail.com>
2020-11-18 19:13:39 +0100

    Add game character model

---
+++ src/models/GameCharacter.ts
@@ -0,0 +1,27 @@
+import * as mongoose from "mongoose";
+
+export interface IGameCharacter extends mongoose.Document{
+    name: string,
+    level: number,
+    healthLevel: number,
+    owner: string,
+    power: number,
+    attackSpeed: number,
+    skills: string,
+    numberOfFights: number,
+    wins: number
+}
+
+const GameCharacterSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  level: { type: Number, required: true, default: 1 },
+  healthLevel: { type: Number, required: true, default: 100 },
+  owner: { type: String, required: true, unique: true },
+  power: {type: Number, required: true, default: 1 },
+  attackSpeed: {type: Number, required: true, default: 1 },
+  skills: { type: String, required: true },
+  numberOfFights: {type: Number, required: true, default: 0 },
+  wins: {type: Number, required: true, default: 0 }
+});
+
+export const GameCharacter = mongoose.model<IGameCharacter>("GameCharacter", GameCharacterSchema);
