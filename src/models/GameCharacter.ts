3caf3f0aa225dc916f78666ad977c4a6733ae580
Maciej Ociepa <masiek457@gmail.com>
2020-12-09 18:04:20 +0100

    Connect Game Character with User v2

--- src/models/GameCharacter.ts
+++ src/models/GameCharacter.ts
@@ -6,11 +6,9 @@ export interface IGameCharacter extends mongoose.Document{
     healthLevel: number,
     power: number,
     attackSpeed: number,
-    ConnectGameCharacterWithUser
-    skills: Array,
     User: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+      userId: string,
     }
     skills: Array<String>,
     numberOfFights: number,
@@ -21,13 +19,12 @@ const GameCharacterSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   level: { type: Number, required: true, default: 1 },
   healthLevel: { type: Number, required: true, default: 100 },
-  owner: { type: String, required: true, unique: true },
   power: { type: Number, required: true, default: 1 },
   attackSpeed: { type: Number, required: true, default: 1 },
   skills: { type: Array },
   numberOfFights: { type: Number, required: true, default: 0 },
-  wins: { type: Number, required: true, default: 0 }
-  User: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  wins: { type: Number, required: true, default: 0 },
+  User: { type: mongoose.Schema.Types.ObjectId, userId: String }
 });
 
 export const GameCharacter = mongoose.model<IGameCharacter>("GameCharacter", GameCharacterSchema);
